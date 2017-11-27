ae4c27685c39ced1d495eff4e70a4a15647772d9
star <qazzaq1230@163.com>
2017-11-27 23:10:04 +0800

    draw line

--- js/day1.js
+++ js/day1.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 
 import { StyleSheet, Text, View, ART } from 'react-native';
+const {Surface, Shape, Path} = ART;
+var Dimensions = require('Dimensions');
+var screenWidth = Dimensions.get('window').width;
 
 export default class Day1 extends Component {
     render() {
-        const path=ART.Path();
-        path.moveTo(1,1);
-        path.lineTo(300,100);
+        var shapes = new Array(6).fill().map((d, i) => {
+            var path = Path().moveTo(0, i * 50 ).lineTo(screenWidth, i * 50 );
+            return <Shape d={path} key={i} stroke='white' strokeWidth={1} strokeDash={[10, 5]}/>;
+        });
+
         return (
-        	 <View style={{
+            <View style={{
                 backgroundColor: 'green',
-                flex:1
+                flex: 1
             }}>
-            <ART.Surface width={300} height={300}>
-            <ART.Shape d={path} stroke='white' strokeWidth={1} />
-            </ART.Surface>
+            <Surface width={screenWidth} height={300}>
+           {shapes}
+            </Surface>
             </View>
         );
     }
