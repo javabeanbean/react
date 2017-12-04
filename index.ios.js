c4a304c6b4894d9e02b84693d2c8b94e02bd1097
star <denghongxing@uniscene.co>
2017-12-04 19:15:59 +0800

    making view

--- index.ios.js
+++ index.ios.js
@@ -6,41 +6,35 @@
 
 import React, { Component } from 'react';
 import codePush from 'react-native-code-push';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import MyImage from './js/pics';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Day1 from './js/day1';
 
 export default class MyApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MyImage/>
-      </View>
-    );
-  }
+    render() {
+        return (
+
+            <Day1/>
+            );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 MyApp = codePush(MyApp);
--- js/day1.js
+++ js/day1.js
@@ -1,27 +1,87 @@
 import React, { Component } from 'react';
 
 import { StyleSheet, Text, View, ART } from 'react-native';
-const {Surface, Shape, Path} = ART;
+const {Surface, Shape, Path,Group} = ART;
 var Dimensions = require('Dimensions');
 var screenWidth = Dimensions.get('window').width;
+var screenHeight = Dimensions.get('window').height;
 
 export default class Day1 extends Component {
     render() {
-        var shapes = new Array(6).fill().map((d, i) => {
-            var path = Path().moveTo(0, i * 50 ).lineTo(screenWidth, i * 50 );
-            return <Shape d={path} key={i} stroke='white' strokeWidth={1} strokeDash={[10, 5]}/>;
-        });
-
+        //两点及半径决定一个圆
+        var leftCircle = Path().moveTo(100, 50).arc(0, 50, 25);
+        var rightCircle = Path().moveTo(screenWidth - 100, 50).arc(0, 50, 25);
         return (
-            <View style={{
-                backgroundColor: 'green',
-                flex: 1
-            }}>
-            <Surface width={screenWidth} height={300}>
-           {shapes}
-            </Surface>
+            <View style={ { backgroundColor: 'white', flex: 1 } }>
+              <View style={ styles.titleContainer }>
+                <Text style={ styles.title }>
+                  mywatch
+                </Text>
+              </View>
+              <View style={ styles.timerContainer }>
+              </View>
+              <View style={ styles.mainContainer }>
+                <Surface
+                         width={ screenWidth }
+                         height={ 150 }>
+                  <Group>
+                    <Shape
+                           d={ leftCircle }
+                           stroke='black'
+                           strokeWidth={ 1 } />
+                    <ART.Text
+                              strokeWidth={ 1 }
+                              stroke='black'
+                              font="10px Heiti SC"
+                              fill='black'
+                              x={ 100 }
+                              y={ 75 }
+                              onLayout={({nativeEvent:e})=>{console.log(e);}}
+                              >
+                      Swipe
+                    </ART.Text>
+                  </Group>
+                  <Shape
+                         d={ rightCircle }
+                         stroke='black'
+                         strokeWidth={ 1 } />
+                </Surface>
+                <View style={ styles.resultContainer }>
+                </View>
+              </View>
             </View>
-        );
+
+
+            );
     }
 }
 
+const styles = StyleSheet.create({
+    titleContainer: {
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+    // textAlign: 'center',
+    //do not work
+    // textAlignVertical: 'bottom',
+    // includeFontPadding :false
+    },
+    timerContainer: {
+        height: 140,
+        borderBottomWidth: 1,
+        borderTopWidth: 1
+    },
+    mainContainer: {
+        flex: 1
+    },
+    resultContainer: {
+        backgroundColor: 'green',
+        flex: 1
+    }
+
+});
