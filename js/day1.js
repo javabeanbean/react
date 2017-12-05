b34688b96b427f6b782000055f28d53b6b29b29e
star <denghongxing@uniscene.co>
2017-12-05 21:09:03 +0800

    add view

--- js/day1.js
+++ js/day1.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 
-import { StyleSheet, Text, View, ART } from 'react-native';
-const {Surface, Shape, Path,Group} = ART;
+import { StyleSheet, Text, View, FlatList, ART } from 'react-native';
+const {Surface, Shape, Path, Group} = ART;
 var Dimensions = require('Dimensions');
 var screenWidth = Dimensions.get('window').width;
 var screenHeight = Dimensions.get('window').height;
 
 export default class Day1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showText: true
+        };
+
+
+    }
+    componentDidMount() {}
     render() {
+        var res = [{
+            name: 1,
+            value: 1
+        }, {
+            name: 2,
+            value: 2
+        }];
+        const offsetX = 100,
+            radius = 50,
+            surfaceHeight = 120,
+            offsetY = (surfaceHeight - radius * 2) / 2;
+        //android need only one arc while ios need two
         //两点及半径决定一个圆
-        var leftCircle = Path().moveTo(100, 50).arc(0, 50, 25);
-        var rightCircle = Path().moveTo(screenWidth - 100, 50).arc(0, 50, 25);
+        var leftCircle = Path().moveTo(offsetX, offsetY).arc(0, radius * 2, radius).arc(0, -radius * 2, radius);
+        var rightCircle = Path().moveTo(screenWidth - offsetX, offsetY).arc(0, radius * 2, radius).arc(0, -radius * 2, radius);
+        //20和10是根据font 10px和字数取的大概宽高
+        var left_font_x = offsetX - 20 / 2;
+        var right_font_x = screenWidth - offsetX - 20 / 2;
+        var font_y = surfaceHeight / 2 - 10 / 2;
         return (
             <View style={ { backgroundColor: 'white', flex: 1 } }>
               <View style={ styles.titleContainer }>
@@ -23,7 +48,7 @@ export default class Day1 extends Component {
               <View style={ styles.mainContainer }>
                 <Surface
                          width={ screenWidth }
-                         height={ 150 }>
+                         height={ surfaceHeight }>
                   <Group>
                     <Shape
                            d={ leftCircle }
@@ -34,19 +59,42 @@ export default class Day1 extends Component {
                               stroke='black'
                               font="10px Heiti SC"
                               fill='black'
-                              x={ 100 }
-                              y={ 75 }
-                              onLayout={({nativeEvent:e})=>{console.log(e);}}
-                              >
-                      Swipe
+                              x={ left_font_x }
+                              y={ font_y }>
+                      计次
                     </ART.Text>
                   </Group>
-                  <Shape
-                         d={ rightCircle }
-                         stroke='black'
-                         strokeWidth={ 1 } />
+                  <Group>
+                    <Shape
+                           d={ rightCircle }
+                           stroke='black'
+                           strokeWidth={ 1 } />
+                    <ART.Text
+                              strokeWidth={ 1 }
+                              stroke='black'
+                              font="10px Heiti SC"
+                              fill='black'
+                              x={ right_font_x }
+                              y={ font_y }>
+                      启动
+                    </ART.Text>
+                  </Group>
                 </Surface>
                 <View style={ styles.resultContainer }>
+                  <FlatList
+                            data={ res }
+                            renderItem={ ({item}) => {
+                                             return (
+                                                 <View style={ styles.resultItem }>
+                                                   <Text>
+                                                     { item.name }
+                                                   </Text>
+                                                   <Text>
+                                                     { item.value }
+                                                   </Text>
+                                                 </View>
+                                                 );
+                                         } } />
                 </View>
               </View>
             </View>
@@ -58,7 +106,7 @@ export default class Day1 extends Component {
 
 const styles = StyleSheet.create({
     titleContainer: {
-        height: 30,
+        height: 50,
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -72,7 +120,7 @@ const styles = StyleSheet.create({
     // includeFontPadding :false
     },
     timerContainer: {
-        height: 140,
+        height: 120,
         borderBottomWidth: 1,
         borderTopWidth: 1
     },
@@ -80,8 +128,18 @@ const styles = StyleSheet.create({
         flex: 1
     },
     resultContainer: {
-        backgroundColor: 'green',
-        flex: 1
+        flex: 1,
+        alignItems: 'flex-end'
+    },
+    resultItem: {
+        width: screenWidth - 10,
+        height: 50,
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: 30,
+        paddingRight: 30
     }
 
 });
